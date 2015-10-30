9b47ff452ef4591b25c4adb481e9fc287a26e795
Mecheri Mohammed <mohammed.mecheri@etu.u-pec.fr>
2015-10-31 00:26:42 +0100

    HoughLines

--- edge.py
+++ edge.py
@@ -3,12 +3,15 @@ import numpy as np
 import math
 scale = 1
 delta = 0
+coefThreshold = 0.5
+distRho = 0.5
+distTheta = 0.01
 ddepth = cv2.CV_16S
 
-img = cv2.imread('dunk.png')
+img = cv2.imread('dunk.jpg')
 height, width = img.shape[:2]
 maxMesure = max(height, width)
-coefThreshold = 0.5
+
 
 img = cv2.GaussianBlur(img, (3, 3), 0) #noise suppression
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) # conversion to grayScale
@@ -33,8 +36,28 @@ cv2.imshow('im_bw',im_bw)
 
 lines = cv2.HoughLines(im_bw, 1, np.pi/180,math.floor(maxMesure*coefThreshold))
 
+
+sumRho = 0
+sumTheta = 0
 for i in range(len(lines)):
-    print(lines[i])
+    sumRho = (lines[i][:, 0] + sumRho)
+    sumTheta = (lines[i][:, 1] + sumTheta)
+moyRho = sumRho / len(lines)
+moyTheta = sumTheta / len(lines)
+print(moyRho)
+print(moyTheta)
+
+for i  in range (len(lines)) :
+    for j in range(i+1, len(lines)):
+        if(((lines[i][0][0] - lines[j][0][0])< moyRho) and ((lines[i][0][1] - lines[j][0][1])< moyTheta)):
+            lines[j][0][0] = 0
+            lines[j][0][1] = 0
+
+
+
+
+for i in range(len(lines)):
+
     for rho, theta in lines[i-1]:
         a = np.cos(theta)
         b = np.sin(theta)
