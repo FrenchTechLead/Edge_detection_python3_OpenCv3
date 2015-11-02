427b9c020cef9f79ed07fb653216345c720dc76e
Mecheri Mohammed <mohammed.mecheri@etu.u-pec.fr>
2015-11-02 22:46:07 +0100

    Trackbars test

--- Edge/Hough.py
+++ Edge/Hough.py
@@ -6,17 +6,17 @@ class Hough:
 
     diagonalOfImage = 0
     coefThreshold = 0
-
+    threshold = 0
     def __init__(self, imgBW, imgColor, coefThreshold ):
         """
         Constructeur de l objet Hough.
         """
         self.inputImageBW = imgBW
-        self.inputImageColor = imgColor
+        inputImageColor = imgColor
         self.coefThreshold = coefThreshold
-        self.height, self.width = imgColor.shape[:2]
+        self.height, self.width = imgColor.shape[:2] # getting the image proprieties
         self.diagonalOfImage = math.sqrt(self.height**2 + self.width**2)
-
+        self.threshold = math.floor(self.diagonalOfImage*self.coefThreshold)
 
 
     def drawLines(self):
@@ -30,10 +30,23 @@ class Hough:
                 b = np.sin(theta)
                 x0 = a*rho
                 y0 = b*rho
-                x1 = int(x0 + 1599*(-b))
-                y1 = int(y0 + 1599*(a))
-                x2 = int(x0 - 1599*(-b))
-                y2 = int(y0 - 1599*(a))
+                x1 = int(x0 + self.diagonalOfImage*(-b))
+                y1 = int(y0 + self.diagonalOfImage*(a))
+                x2 = int(x0 - self.diagonalOfImage*(-b))
+                y2 = int(y0 - self.diagonalOfImage*(a))
 
+                print("x1 = "+str(x1)+" y1 = "+str(y1))
+                print("x2 = "+str(x2)+" y2 = "+str(y2)+"\n")
                 cv2.line(self.inputImageColor, (x1, y1), (x2, y2), (0, 0, 255), 1)
         return self.inputImageColor
+
+    def drawLinesP(self, minLineLenght, maxLineGap, coloredImage, thresholdOptional = threshold):
+        lines = cv2.HoughLinesP(self.inputImageBW,1,np.pi/180,thresholdOptional,minLineLenght, maxLineGap)
+        for i in range(len(lines)):
+            for x1,y1,x2,y2 in lines[i-1]:
+                cv2.line(coloredImage, (x1, y1), (x2, y2),(0,255,0),1)
+                #print("x1 = "+str(x1)+" y1 = "+str(y1))
+                #print("x2 = "+str(x2)+" y2 = "+str(y2)+"\n")
+                #print(self.threshold)
+                print("minLineLenght = "+str(minLineLenght)+" maxLineGap = "+str(maxLineGap)+" threshold = "+str(thresholdOptional))
+        return coloredImage
--- Main.py
+++ Main.py
@@ -4,18 +4,47 @@ import numpy as np
 from Edge.Sobel import *
 from Edge.Hough import *
 
+def nothing(x):
+    pass
+inputImage = cv2.imread('dunk.png')
+inputImage1 = cv2.imread('dunk.png')
+cv2.namedWindow('fenetre')
 
-img = cv2.imread('dunk.png') # importing in grayScale Mode
+cv2.createTrackbar('Threshold','fenetre',300,300,nothing)
+cv2.createTrackbar('MinLineLenth','fenetre',300,300,nothing)
+cv2.createTrackbar('MaxLineGape','fenetre',100,100,nothing)
 
-imgSobel = mySobel(img, 1)
+switch = '0 : OFF \n1 : ON'
+cv2.createTrackbar(switch, 'fenetre',1,1,nothing)
 
-linedImageObject = Hough(imgSobel,img, 0.4)
-
-linedImage = linedImageObject.drawLines()
-
+imgSobel = mySobel(inputImage, 1) # second parameter refers to ksize
 
 
+threshold = 0
+minLineLenth = 0
+maxLineGape = 0
+while(1):
 
-cv2.imshow('linedImage', linedImage)
+    k = cv2.waitKey(1) & 0xFF
+    if k == 27:
+        break
+    # get current positions of four trackbars
+    threshold = cv2.getTrackbarPos('Threshold','fenetre')
+    minLineLenth = cv2.getTrackbarPos('MinLineLenth','fenetre')
+    maxLineGape = cv2.getTrackbarPos('MaxLineGape','fenetre')
+    s = cv2.getTrackbarPos(switch,'fenetre')
+
+
+
+    if s == 0:
+        linedImage = linedImageObject.drawLinesP(1,10)
+    else:
+        inputImage1 = inputImage
+        linedImageObject = Hough(imgSobel,inputImage1, 0.25)# third parameter refers to Threashold Coef
+        linedImage = linedImageObject.drawLinesP( minLineLenth, maxLineGape,inputImage1,threshold)
+        cv2.imshow('fenetre', linedImage)
+        cv2.imshow('Sobel', imgSobel)
+        cv2.imshow('inputImage1', inputImage1)
+        cv2.imshow('inputImage', inputImage)
+
 cv2.waitKey(0)
-cv2.destroyAllWindows()
